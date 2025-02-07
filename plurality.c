3b649b668aa4957aa505ea961f8716a2010c40ee
Matheus Tito <mthsilva0@gmail.com>
2025-02-07 07:09:40 -0300

    feat: added comments

--- plurality.c
+++ plurality.c
@@ -11,7 +11,7 @@ char **voting(char *candidates[], int candidatesCount, int votersCount);
 int voteCounting(char **votes, int votersCount, char *candidates[], int candidatesCount);
 
 int main(int argc, char *argv[]) {
-    int candidatesCount = argc - 1;
+    int candidatesCount = argc - 1; //If just argc, it will consider also the calling of the function
     int votersCount = 0;
 
     if (argc <= 2) {
@@ -24,6 +24,7 @@ int main(int argc, char *argv[]) {
         return 2;
     }
 
+    //If invalid input, keep scanning.
     printf("Number of voters: ");
     while (scanf("%d",&votersCount) != 1 || votersCount <= 1 || votersCount > MAX_VOTERS) {
         printf("Invalid input.\nPlease enter a valid number of voters: ");
@@ -32,16 +33,20 @@ int main(int argc, char *argv[]) {
     
     char *candidates[99];
 
+    //Pass down the candidates to an array insted of using argv.
     for (int i = 0; i < candidatesCount; i++) {
         candidates[i] = argv[i + 1];
     }
 
+    //Receives the returning array from voting.
     char **votes = voting(candidates, candidatesCount, votersCount);
 
     if (votes == NULL) {
+        printf("No valid votes.");
         return 1;
     }
 
+    //Calls the voteCouting function to read votes and print the winner
     voteCounting(votes, votersCount, candidates, candidatesCount);
 
     for (int i = 0; i < votersCount; i++) {
@@ -53,9 +58,10 @@ int main(int argc, char *argv[]) {
 }
 
 char **voting(char *candidates[], int candidatesCount, int votersCount) {
-    char **votes = malloc(votersCount * sizeof(char *)); //[MAX_VOTERS];
+    char **votes = malloc(votersCount * sizeof(char *)); //Allocate the memory to the quantity of voters, each of size of a char pointer.
 
     for (int i = 0; i < votersCount; i++) {
+        //Alocate memory to the voting indexes.
         votes[i] = malloc(MAX_NAME_LENGTH * sizeof(char));
         if (votes[i] == NULL) {
             printf("Memory allocation failed!\n");
@@ -63,6 +69,7 @@ char **voting(char *candidates[], int candidatesCount, int votersCount) {
         }
     }
 
+    //Read votes
     for (int i = 0; i < votersCount; i++) {
         printf("Eleitor %d. Voto: ", i + 1);
         scanf("%s", votes[i]);
@@ -88,20 +95,20 @@ char **voting(char *candidates[], int candidatesCount, int votersCount) {
 
 int voteCounting(char **votes, int votersCount, char *candidates[], int candidatesCount) {
     int frequency;
-    char result[30][MAX_NAME_LENGTH];
+    char result[99][MAX_NAME_LENGTH];
     int resultCount = 0;
 
     for(int i = 0; i < candidatesCount; i++) {
         int count = 0;
 
         for(int j = 0; j < votersCount; j++) {
-            //printf("%s", votes[j]);
-
+            //Count each vote based on the candidate that is receiving it.
             if (strcmp(votes[j], candidates[i]) == 0) {
                 count++;
             }
         }
 
+        //If it appears more than the actual frequency, then add the candidate to the result array and starts the resultCount.
         if(count > frequency) {
             frequency = count;
             resultCount = 0;
@@ -116,6 +123,7 @@ int voteCounting(char **votes, int votersCount, char *candidates[], int candidat
                 }
             }
 
+            //If it isn't a dpulicate, then the candidate will also go to the array of results.
             if(!isDuplicate) {
                 strcpy(result[resultCount], candidates[i]);
                 resultCount++;
@@ -124,20 +132,20 @@ int voteCounting(char **votes, int votersCount, char *candidates[], int candidat
     }
 
     if (resultCount > 1) {
-        printf("Os candidatos com mais votos foram: ");
+        printf("The winner candidates are: ");
         for (int i = 0; i < resultCount; i++) {
             printf("%s ", result[i]);
         }
-        printf("\nEles receberam %d votos.", frequency);
+        printf("\nThey received %d votes.", frequency);
 
         return 0;
     }
 
-    printf("O candidato que ganhou foi o ");
+    printf("The winner candidate is ");
     for (int i = 0; i < resultCount; i++) {
         printf("%s ", result[i]);
     }
-    printf("\nEle recebeu %d votos.", frequency);
+    printf("\nHe received %d votes.", frequency);
 
     return 0;
 }
